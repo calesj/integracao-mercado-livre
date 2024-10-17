f818c77d3aeab28a7bfd54fe647b5f67ec69d65d
cales <cales.junes@me2.com.br>
2024-10-16 23:08:21 -0300

    feat - adicionando a rota dashboard para usuarios logados

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
-use App\Http\Controllers\AuthenticateController;
+use App\Http\Controllers\Auth\AuthenticateController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthenticateController::class, 'index']);
+Route::get('login', [AuthenticateController::class, 'create'])->name('login');
+Route::post('login', [AuthenticateController::class, 'store'])->name('login.store');
 
 Route::get('ml-access', [AuthenticateController::class, 'accessToken']);
+
+Route::group(['middleware' => ['web', 'auth']], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+});
+
+include __DIR__.'/auth.php';
+include __DIR__.'/mercadolivre.php';
